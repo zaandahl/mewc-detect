88a194e1e6d705d7163b5d85799af8f9942096be
zaandahl <zaandahl@users.noreply.github.com>
2022-08-15 15:18:07 +1000

    adding spaces to command string

--- src/megadetector.py
+++ src/megadetector.py
@@ -9,15 +9,15 @@ def read_yaml(file_path):
         return yaml.safe_load(f)
 
 def create_command(config):
-    if(config["RECURSIVE"]): recursive_str = "--recursive"
+    if(config["RECURSIVE"]): recursive_str = " --recursive"
     else: recursive_str = ""
-    if(config["RELATIVE_FILENAMES"]): rel_filename_str = "--output_relative_filenames"
+    if(config["RELATIVE_FILENAMES"]): rel_filename_str = " --output_relative_filenames"
     else: rel_filename_str = ""
     threshold_str = " --threshold=" + str(config["THRESHOLD"])
     checkpoint_freq_str = " --checkpoint_frequency=" + str(config["CHECKPOINT_FREQ"])
-    if(config["CHECKPOINT_FILE"] is not None): res_checkpoint_str = "--resume_from_checkpoint " + config["CHECKPOINT_FILE"]
+    if(config["CHECKPOINT_FILE"] is not None): res_checkpoint_str = " --resume_from_checkpoint " + config["CHECKPOINT_FILE"]
     else: res_checkpoint_str = ""
-    if(config["NCORES"] is not None): ncores_str = "--ncores " + config["NCORES"]
+    if(config["NCORES"] is not None): ncores_str = " --ncores " + config["NCORES"]
     detector_file_str = " /code/" + str(config["MD_MODEL"]) + " "
     image_file_str = str(config["INPUT_DIR"]) + "/" + str(config["IMG_FILE"]) + " "
     output_file_str = str(config["INPUT_DIR"]) + "/" + str(config["MD_FILE"])
